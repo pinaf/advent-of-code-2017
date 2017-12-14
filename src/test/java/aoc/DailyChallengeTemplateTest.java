ebcda1242d615b50b6ae5a631249c498f0317a36
Felipe Pina <felipe.pina@protonmail.com>
2017-12-13 22:21:20 -0200

    - cleanup

---
+++ src/test/java/aoc/DailyChallengeTemplateTest.java
@@ -0,0 +1,22 @@
+package aoc;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class DailyChallengeTemplateTest {
+
+    @Test
+    public void part1() {
+        MatcherAssert.assertThat(new DailyChallengeTemplate("1").part1(), Matchers.is(0L));
+    }
+
+    @Test
+    public void part2() {
+        MatcherAssert.assertThat(new DailyChallengeTemplate("1").part2(), Matchers.is(0L));
+    }
+
+}
